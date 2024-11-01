24d5541edbca5fed5f1c12301add386394c22e75
zigazaga4 <160751634+zigazaga4@users.noreply.github.com>
2024-11-01 22:05:29 +0200

    Update Esp.h

--- Assault cube hack/Esp.h
+++ Assault cube hack/Esp.h
@@ -4,6 +4,6 @@
 #include "Memory reader.h"
 #include "Directx.h"
 #include "Utils.h"
-#include "Globals.h"  // Add this line
+#include "Globals.h"
 
 void RenderESP(ID2D1DeviceContext* pRenderTarget, ID2D1SolidColorBrush* pBrush, const viewMatrix& viewMatrix, const std::vector<Player>& players, int screenWidth, int screenHeight, float cameraYaw, float cameraPitch);
